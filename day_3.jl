5e8c4c8f3f830a1e294e1a56833ad7a617314caf
Pavel Okunev <p.okunev@corp.mail.ru>
2021-12-03 17:11:20 +0300

    some improvements

--- day_3.jl
+++ day_3.jl
@@ -1,4 +1,4 @@
-parseLine(l::String) = split(l, "") .|> c -> parse(Int, c)
+parseLine(l::String) = collect(l) .|> c -> parse(Bool, c)
 
 function bitarr_to_int(arr::AbstractVector{Bool})
 	result = 0
@@ -12,36 +12,35 @@ end
 
 most_common_bit(r::AbstractVector{Bool}) = sum(r) >= length(r) / 2
 
-find_idxs(v1::AbstractMatrix, v2::AbstractVector) = [idx for (idx,r) in enumerate(eachrow(v1)) if r == v2]
+find_idxs(v1::AbstractMatrix, v2::AbstractVector) = [idx for (idx,r) in enumerate(eachcol(v1)) if r == v2]
 
-function find_rating(vectors::BitMatrix, most_common_bit_mode=true)
+function find_rating(vectors::AbstractMatrix{Bool}, most_common_bit_mode=true)
 	prefix_size = 1
 	match_vector::BitVector = []
-	while size(vectors, 1) > 1
-		prefix_column = @view vectors[:,prefix_size]
+	while size(vectors, 2) > 1
+		prefix_column = @view vectors[prefix_size, :]
 		next_bit = most_common_bit(prefix_column)
 		if !most_common_bit_mode
 			next_bit = !next_bit
 		end
-
 		push!(match_vector, next_bit)
-		match_idxs = find_idxs((@view vectors[:,1:prefix_size]), match_vector)
+		match_idxs = find_idxs((@view vectors[1:prefix_size,:]), match_vector)
 		prefix_size += 1
-		vectors = vectors[match_idxs, :]
+		vectors = vectors[:,match_idxs]
 	end	
-	bitarr_to_int(@view vectors[1,:])
+	bitarr_to_int(@view vectors[:,1])
 end
 
 
 function main1()
-	vectors = readlines("day_3.txt") .|> parseLine |> v -> hcat(v...) |> permutedims |> BitMatrix
-	gamma_rate = most_common_bit.(eachcol(vectors))
+	vectors = readlines("day_3.txt") .|> parseLine |> v -> hcat(v...)
+	gamma_rate = most_common_bit.(eachrow(vectors))
 	epsilon_rate = .~(gamma_rate)
 	bitarr_to_int(gamma_rate) * bitarr_to_int(epsilon_rate)
 end
 
 function main2()
-	vectors = readlines("day_3.txt") .|> parseLine |> v -> hcat(v...) |> permutedims |> BitMatrix
+	vectors = readlines("day_3.txt") .|> parseLine |> v -> hcat(v...)
 	oxygen_rating = find_rating(vectors)
 	co2_rating = find_rating(vectors, false)
     oxygen_rating * co2_rating
