255db71cf0a0e66eccc8e20125e28051fcb1ab00
rodrask <rodrask@protonmail.com>
2021-12-11 14:50:07 +0300

    in-place solution

--- day_11.jl
+++ day_11.jl
@@ -6,34 +6,30 @@ const f = CartesianIndex(1,1)
 neighbours(idx::CartesianIndex{2}, l::CartesianIndex{2}) = [i for i in max(f, idx-f):min(l, idx+f) if i!=idx]
 neighboursFunction(m::AbstractMatrix) = myneighbours(idx::CartesianIndex{2}) = neighbours(idx, CartesianIndex(size(m)))
 
-function step(octomap::Matrix{Int}, myneighbours::Function)
-    result = octomap .+ 1
+function step!(octomap::Matrix{Int}, myneighbours::Function)
+    octomap .+=  1
     flashes = Set{CartesianIndex{2}}()
-    flashstack = [idx for idx in CartesianIndices(octomap) if result[idx] == FLASH]
+    flashstack = [idx for idx in CartesianIndices(octomap) if octomap[idx] == FLASH]
     while !isempty(flashstack)
         flashidx = pop!(flashstack)
         push!(flashes, flashidx)
         flashneighbours = myneighbours(flashidx)
         for nidx in flashneighbours
-            nvalue = result[nidx] += 1
+            nvalue = octomap[nidx] += 1
             if nvalue == FLASH && nvalue ∉ flashes
                 push!(flashes, nidx)
                 push!(flashstack, nidx)
             end
         end
     end
-    result[collect(flashes)] .= 0
-    result, length(flashes)
+    octomap[collect(flashes)] .= 0
+    length(flashes)
 end
 
 function main1()
     octomap = readMap("day_11.txt")
     myneighbours = neighboursFunction(octomap)
-    total = 0
-    for _ in 1:100
-        octomap, nflashes = step(octomap, myneighbours)
-        total += nflashes
-    end
+    total = sum((step!(octomap, myneighbours) for _ in 1:100))
     println("Total $total flashes")
 end
 
@@ -41,10 +37,9 @@ function main2()
     octomap = readMap("day_11.txt")
     mapsize = prod(size(octomap))
     myneighbours = neighboursFunction(octomap)
-    nstep = nflashes = 0
-    while nflashes != mapsize
+    nstep = 1
+    while step!(octomap, myneighbours) != mapsize
         nstep += 1
-        octomap, nflashes = step(octomap, myneighbours)
     end
     println("Synchronize at step $nstep")
 end
