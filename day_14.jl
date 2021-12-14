7356750a61eec1d28a5fef5094454afcb1972359
rodrask <rodrask@protonmail.com>
2021-12-15 00:06:35 +0400

    day 14 solution

--- day_14.jl
+++ day_14.jl
@@ -7,27 +7,44 @@ end
 
 loadProductionRules(io::IOStream) = eachline(io) .|> parseline |> Dict
 
-function applyRules(template::AbstractVector{Char}, rules::Dict{Tuple{Char,Char}, Char})
-    result = flatten((f1, rules[(f1,f2)]) for (f1,f2) in zip(template, template[2:end]))
-    append!(result, [template[end]])
-    result
-end
+increment!(counter::Dict{T, Int}, key::T, value::Int=1) where T = counter[key] = value + get(counter, key, 0)
 
-function score(template::Vector{Char})
+function applyRulesAndScore(template::AbstractVector{Char}, rules::Dict{Tuple{Char,Char}, Char}, steps::Int)
+    paircounter = Dict{Tuple{Char, Char}, Int}()
     counter = Dict{Char, Int}()
-    foreach(ch -> counter[ch] = 1 + get(counter, ch, 0), template)
-    counter |> findmax |> first - counter |> findmin |> first
+    foreach(ch -> increment!(counter,ch), template)
+    foreach(pair -> increment!(paircounter,pair), zip(template, template[2:end]))
+    for _ in 1:steps
+        nextpaircounter = Dict{Tuple{Char, Char}, Int}()
+        for ((p1,p2), c) in pairs(paircounter)
+            newchar = rules[(p1,p2)]
+            increment!(counter, newchar, c)
+            increment!(nextpaircounter, (p1,newchar), c)
+            increment!(nextpaircounter, (newchar,p2), c)
+        end
+        paircounter = nextpaircounter
+    end
+    (counter |> findmax |> first) - (counter |> findmin |> first)
 end
 
-steps = 10
-function main1() 
-    template, rules = open("day_14_test.txt") do io
+function readInput(path)
+    open(path) do io
         template = readline(io) |> collect
         readline(io)
         template, loadProductionRules(io)
     end
-    template = foldl((t, _)-> applyRules(t, rules), 1:steps;init=template)
-    score(template)
+end
+
+
+function main1()
+    template, rules = readInput("day_14_test.txt")
+    println(applyRulesAndScore(template, rules, 10))
+end
+
+function main2()
+    template, rules = readInput("day_14.txt")
+    println(applyRulesAndScore(template, rules, 40))
 end
 
 main1()
+main2()
