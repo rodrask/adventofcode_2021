9209f6210d58c27c857466fc198d8ba2d4a33eb3
rodrask <rodrask@protonmail.com>
2021-12-14 10:46:10 +0300

    replace readline

--- day_13.jl
+++ day_13.jl
@@ -1,12 +1,13 @@
 function loadSheet(io::IOStream)
     indicies = CartesianIndex{2}[]
     rows, cols = 0, 0
-    line = readline(io)
-    while !isempty(line)
+    for line in eachline(io)
+        if isempty(line)
+            break
+        end
         c, r = split(line, ",") .|> v -> parse(Int, v)
         push!(indicies, CartesianIndex(r+1,c+1))
         rows, cols = max(rows, r+1), max(cols, c+1)
-        line = readline(io)
     end
     result = falses(rows, cols)
     result[indicies] .= true
@@ -26,7 +27,7 @@ parsefold(rm::RegexMatch) = rm[:dir]=="x" ? VerticalFold(parse(Int, rm[:axis])+1
 const foldregex = r"fold along (?<dir>[xy])=(?<axis>\d+)"
 loadInstructions(io::IOStream) = [parsefold(match(foldregex, line)) for line in readlines(io)]
 
-printSheet(sheet::BitMatrix) = foreach(row -> println(join([v ? '0' : '.' for v in row])), eachrow(sheet))
+printSheet(sheet::BitMatrix) = foreach(row -> println(join([v ? '*' : ' ' for v in row])), eachrow(sheet))
 
 applyFold(m::BitMatrix, f::VerticalFold) =   (@view m[:, 1:(f.axis-1)]) .| (@view m[:, end:-1:(f.axis+1)])
 applyFold(m::BitMatrix, f::HorizontalFold) = (@view m[1:(f.axis-1), :]) .| (@view m[end:-1:(f.axis+1), :])
