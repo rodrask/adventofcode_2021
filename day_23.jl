457339aeb7b0a8e3c6ce4ca5ca0364c27e04bbec
rodrask <rodrask@protonmail.com>
2021-12-24 10:36:25 +0300

    day 23 part 1

--- day_23.jl
+++ day_23.jl
@@ -1,22 +1,30 @@
-using Memoize, DataStructures
+using Memoize, DataStructures, Parameters
 Position = NamedTuple{(:y, :x), Tuple{Int,Int}}
 Amphipod = NamedTuple{(:name, :pos), Tuple{Char,Position}}
 State = Dict{Char, Vector{Position}}
 
-hallx = [1,2,4,6,8,10,11]
-hall = [(y=1,x=x) for x in hallx]
+@with_kw struct World 
+	roomlevels::Vector{Int}
 
-roomsx = [3,5,7,9]
-roomlevels = (2,3)
-room(x::Int) = [(y=l,x=x) for l in roomlevels]
-rooms = vcat([room(x) for x in roomsx]...)
-roomhall(room::Position) = (y=1,x=room[:x])
+	hallx::Vector{Int} = [1,2,4,6,8,10,11]
+	hall::Vector{Position} = [(y=1,x=x) for x in hallx]
+	roomsx::Vector{Int} = [3,5,7,9]
+	amphipods = ['A','B','C','D']
+	weights = Dict(a=>10^(i-1) for (i,a) in enumerate(amphipods))
+end
 
-burrow = vcat(hall, rooms)
+@memoize room(w::World, x::Int) = [(y=l,x=x) for l in w.roomlevels]
+@memoize rooms(w::World) = vcat([room(w,x) for x in w.roomsx]...)
 
-amphipods = ['A','B','C','D']
-weights = Dict(a=>10^(i-1) for (i,a) in enumerate(amphipods))
-finalrooms = Dict(a=>room(rx) for (a,rx) in zip(amphipods, roomsx))
+@memoize lowerrooms(w::World, pos::Position) = [r for r in room(w,pos[:x]) if r[:y] > pos[:y]]
+@memoize upperrooms(w::World, pos::Position) = [r for r in room(w,pos[:x]) if r[:y] < pos[:y]]
+
+@memoize finalrooms(w::World) = Dict(a=>room(w, rx) for (a,rx) in zip(w.amphipods, w.roomsx))
+
+isinhall(pos::Position) = pos[:y] == 1
+
+w1 = World(roomlevels=[3,2])
+w2 = World(roomlevels=[5,4,3,2])
 
 function directsteps(from::Int, to::Int)
 	step = from < to ? 1 : -1
@@ -28,8 +36,9 @@ end
 		return Position[]
 	end
 	sameroom = from[:x] == to[:x]
-	fromhall = from[:x] == 1
-	tohall = to[:x] == 1
+	fromhall = isinhall(from)
+	tohall = isinhall(to)
+
 	if sameroom
 		return [(y=y,x=from[:x]) for y in directsteps(from[:y], to[:y])] 
 	elseif fromhall & tohall
@@ -47,47 +56,55 @@ end
 	end
 end
 
-@memoize pathcost(name::Char, path::Vector{Position}) = length(path) * weights[name]
+@memoize pathcost(weight::Int, path::Vector{Position}) = length(path) * weight
 
-occupied(s::State) = vcat(values(s)...)
-candidates(s::State) = filter(b -> b ∉ occupied(s), burrow)
-
-isblocked(s::State, pos::Position) = (pos[:y] == 3) && (y=2, x=pos[:x]) ∈ occupied(s)
+@memoize occupied(s::State) = vcat(values(s)...)
+isblocked(w::World, s::State, pos::Position) = (pos[:y] > 2) && !isempty(upperrooms(w, pos) ∩ occupied(s))
 isfeasible(state::State, path::Vector{Position}) = isempty(occupied(state) ∩ path)
 
-@memoize isfinal(name::Char, pos::Position) =  pos ∈ finalrooms[name]
-@memoize isdeepfinal(name::Char, pos::Position) = isfinal(name, pos) && (pos[:y] == 3)
 
-isfinal(state::State) = all(isfinal(name, p1) && isfinal(name, p2) for (name, (p1, p2)) in pairs(state))
+@memoize isfinal(w::World, name::Char, pos::Position, filledpos::Vector{Position}) = 
+		(pos ∈ finalrooms(w)[name]) && (lowerrooms(w, pos) ⊆ filledpos)
 
-@memoize function mincost(state::State)
+@memoize function mincost(w::World, state::State)
 	result = 0
-	for (name, (p1,p2)) in pairs(state)
-		# println(name," ", p1, " ", p2)
-		f1,f2 = finalrooms[name]
-		# println(f1, " ", f2)
-		# println(pathcost(name, path(p1,f1)), " ",pathcost(name, path(p2,f2)))
-		# println(pathcost(name, path(p1,f2)), " ",pathcost(name, path(p2,f1)))
-		result += min(pathcost(name, path(p1,f1)) + pathcost(name, path(p2,f2)),
-			pathcost(name, path(p1,f2)) + pathcost(name, path(p2,f1)))
+	for (name, positions) in pairs(state)
+		weight = w.weights[name]
+		fhighest = finalrooms(w)[name][end]
+		misplaced = 0
+		for pos in positions
+			if !isfinal(w, name, pos, positions)
+				result += (pathcost(weight, path(pos, fhighest)) + misplaced)
+				misplaced += 1
+			end
+		end
+
+		# result += min(pathcost(weight, path(p1,f1)) + pathcost(weight, path(p2,f2)),
+		# 	pathcost(weight, path(p1,f2)) + pathcost(weight, path(p2,f1)))
 	end
 	result
 end
 
-function nextmoves(state::State)
-	cands = candidates(state)
+function nextmoves(w::World, state::State)
 	result = []
-	for name in amphipods
-		ps = state[name]
-		if all(isfinal(name, p) for p in ps)
-			continue
-		end
-		for (idx, p) in zip((1,2), ps)
-			if isblocked(state, p) || isdeepfinal(name, p)
+	currentpositions = occupied(state)
+	for name in reverse(w.amphipods)
+		positions = state[name]
+		for (idx, pos) in enumerate(positions)
+			if isblocked(w, state, pos) || isfinal(w, name, pos, positions)
 				continue
 			end
-			pathcandidates = cands |> cs -> map(c -> path(p, c), cs) |> paths -> filter(p -> isfeasible(state, p), paths)
-			append!(result, [(name, idx, path[end], pathcost(name,path)) for path in pathcandidates])
+			for finalroom in finalrooms(w)[name]
+				frpath = path(pos, finalroom)
+				if isfeasible(state, frpath)
+					push!(result, (name, idx, finalroom, pathcost(w.weights[name], frpath)))
+				end
+			end
+			if !isinhall(pos)
+				hallpaths = [path(pos, h) for h in setdiff(w.hall, currentpositions)]
+				hallmoves = [(name, idx, hp[end], pathcost(w.weights[name], hp)) for hp in hallpaths if isfeasible(state, hp)]
+				append!(result, hallmoves)
+			end
 		end
 	end
 	result
@@ -96,73 +113,87 @@ end
 function domove(s::State, name::Char, idx::Int, to::Position)
 	result = deepcopy(s)
 	result[name][idx] = to
+	sort!(result[name])
 	result
 end
 
 
-function Astar(state::State, maxsteps::Int=100, io::IO=stdout)
+function Astar(w::World, state::State; io::IO=stdout)
 	bestsofar = nothing
 	bestscore = typemax(Int)
-	queue = PriorityQueue{State, Int}()
-	queue[state] = 0
-	step = 1
-	realscore = 12521
-	while !isempty(queue) && step < maxsteps
-		state, scost = peek(queue)
-		delete!(queue, state)
-		for (name, idx, to, cost) in nextmoves(state)
+	costdict = Dict{State, Int}([state=>0])
+	queue = PriorityQueue{State, Int}([state=>mincost(w, state)])
+	step = 0
+	while !isempty(queue)
+		step += 1
+		state = dequeue!(queue)
+		scost = costdict[state]
+		for (name, idx, to, cost) in nextmoves(w, state)
 			maybenext = domove(state, name, idx, to)
-			costestimate = mincost(maybenext)
-			nextscore = scost + cost + costestimate
-
-			if costestimate == 0 && nextscore <= bestscore
+			nextscore = scost + cost
+			costestimate = mincost(w, maybenext)
+			if costestimate == 0 && nextscore < bestscore
 				bestscore = nextscore
 				bestsofar = maybenext
 				println(io, "found final with score ", nextscore)
-			elseif nextscore <= min(bestscore, get(queue, maybenext, typemax(Int)), realscore)
-				queue[maybenext] = nextscore
-				show(io, maybenext)
-				println(io, "Moving ",name," to ",to)
-				println(io, "State score: ", scost + cost," cost estimate ", costestimate, " total ",nextscore, "\n")
+			elseif nextscore + costestimate < bestscore && nextscore < get(costdict, maybenext, typemax(Int))
+				costdict[maybenext] = nextscore
+				queue[maybenext] = nextscore + costestimate
+				# println(io, "Moving ",name," to ",to, " step ", step, " State score: ", scost + cost, " total ",nextscore)
+				# show(io, maybenext, w)
+			else
+				# println(io, "Reject step ",name," to ",to, "State score: ", scost + cost, " total ",nextscore)
+				# show(io, maybenext, w)
 			end
 		end
-		step += 1
 	end
-	flush(io)
 	bestscore
 end
 
-function load(strmap::Vector{String})
+function insert_part2(strmap::Vector{String})
+	append!(strmap[1:3],["  #D#C#B#A#","  #D#B#A#C#"],strmap[4:5]) 
+end
+
+function load(strmap::Vector{String}, w::World=w1)
 	result = Dict{Char, Vector{Position}}()
 	shift = 1
-	for x in hallx
+	for x in w.hallx
 		roompod = strmap[2][shift+x]
-		if roompod ∈ amphipods
+		if roompod ∈ w.amphipods
 			push!(get!(result, roompod, Position[]), (y=1,x=x))
 		end
 	end
-	for x in roomsx
-		for y in roomlevels
+	for x in w.roomsx
+		for y in w.roomlevels
 			roompod = strmap[shift+y][shift+x]
-			if roompod ∈ amphipods
+			if roompod ∈ w.amphipods
 				push!(get!(result, roompod, Position[]), (y=y,x=x))
 			end
 		end
 	end
+	foreach(sort!, values(result))
 	State(result)
 end
 
 toci(p::Position) = CartesianIndex(p[:y],p[:x]+1)
 
-function show(io::IO, s::State)
-	burrow = fill('#', (3,13))
+show(s::State, w::World=w1) = show(stdout, s, w)
+function show(io::IO, s::State, w::World=w1)
+	height = length(w.roomlevels) + 1
+	burrow = fill('#', (height,13))
 	burrow[1,2:end-1] .= '.'
-	burrow[rooms .|> toci] .= '.'
-	burrow[1:3,1] = ['1','2','3']
-	for (name, (p1,p2)) in pairs(s)
-		burrow[[p1,p2] .|> toci] .= name
+	burrow[rooms(w) .|> toci] .= '.'
+	burrow[1:height,1] = 1:height |> join |> collect
+	for (name, positions) in pairs(s)
+		burrow[positions .|> toci] .= name
 	end
 	println(io, "#12345678901#")
 	[println(io, join(r)) for r in eachrow(burrow)]
 	println(io, "  #########")
+end
+
+function runwithlogs(s::State, path="test.out",w::World=w1)
+	open(path,"w") do io
+		Astar(w, s, io=io)
+	end
 end
